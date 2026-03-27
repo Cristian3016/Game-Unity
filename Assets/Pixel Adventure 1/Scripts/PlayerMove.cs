628dc9a2169ba2c7a3a9a54f06aef44f13b51f3d
Cristian3016 <cristiancaro163@gmail.com>
2026-03-27 16:24:31 -0300

    velocity X y Jump

--- Assets/Pixel Adventure 1/Scripts/PlayerMove.cs
+++ Assets/Pixel Adventure 1/Scripts/PlayerMove.cs
@@ -1,16 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float runSpeed = 2;
+
+    public float jumpSpeed = 3;
+
+    Rigidbody2D rb2D;
+
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.linearVelocity = new Vector2(runSpeed, rb2D.linearVelocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.linearVelocity = new Vector2(-runSpeed, rb2D.linearVelocity.y);
+        }
+        else
+        {
+            rb2D.linearVelocity = new Vector2(0, rb2D.linearVelocity.y);
+        }
+        if(Input.GetKey("space") && CheckGround.isGrounded)
+        {
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpSpeed);
+        }
+
+
     }
 }
